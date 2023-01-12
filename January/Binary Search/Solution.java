76fc798d5517ef692c56bfbeec4408ab2f68df6c
mathewlg <mathewlopezg@gmail.com>
2023-01-11 22:10:42 -0600

    Add solution for binary search

---
+++ January/Binary Search/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int middle = 0;
+        while(left <= right){
+            middle = left + (right - left) / 2;
+            if(nums[middle] == target){
+                return middle;
+            }else{
+                if(nums[middle] > target){
+                    right = middle -1;
+                }else{
+                    left = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
