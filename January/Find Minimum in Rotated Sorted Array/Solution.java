38fac679e77487acbe57b4d344b82d58cd6212a1
mathewlg <mathewlopezg@gmail.com>
2023-01-13 21:29:10 -0600

    Solve find minimum in rotated sorted array

---
+++ January/Find Minimum in Rotated Sorted Array/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int current = 0;
+        int prev = 9999;
+        int result = -1;
+        for(int i = 0; i < nums.length; i++){
+            current = nums[i];
+            if(i > 0){
+                prev = nums[i-1];
+            }
+            if(current < prev){
+                result = current;
+            }
+        }
+        return result;
+    }
+}
